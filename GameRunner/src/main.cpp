a6e4d10be5b4bda8daf5dcd02f0915410f016ce6
Zavertkin_KN <kostyanz2595@gmail.com>
2022-06-07 01:55:08 +0300

    Промежуточный коммит

--- GameRunner/src/main.cpp
+++ GameRunner/src/main.cpp
@@ -30,6 +30,60 @@ using namespace game_backend;
 
 //! TODO Добавить выборочный запуск
 
+void runSnake( TRenderer& renderer, TRectangleDescription& activeGameField, TRectangleDescription& background, const string& fontFile, size_t fontSize )
+{
+    //! Cells params
+    TCellsFieldParams cellsFieldParams;
+    cellsFieldParams.xCellsCount = 20;
+    cellsFieldParams.yCellsCount = 20;
+    cellsFieldParams.cellHeight = 25;
+    cellsFieldParams.cellWidth = 25;
+
+    TRectangleDescription gameFieldBound;
+    gameFieldBound.xStart = activeGameField.xStart - 1;
+    gameFieldBound.yStart = activeGameField.yStart - 1;
+    gameFieldBound.width = cellsFieldParams.xCellsCount * cellsFieldParams.cellWidth + 2;
+    gameFieldBound.height = cellsFieldParams.yCellsCount * cellsFieldParams.cellHeight + 2;
+    gameFieldBound.isFilled = false;
+    gameFieldBound.color = { 0xFF, 0xFF, 0xFF, 0xFF };
+
+    TCellRectangles mainFieldCellsGrid;
+    mainFieldCellsGrid.setCellsFieldParams( activeGameField, cellsFieldParams );
+    mainFieldCellsGrid.calcGrid();
+
+    //! Game backend
+    TSnakeGame snakeGame( { cellsFieldParams.yCellsCount, cellsFieldParams.xCellsCount }, 5 );
+
+    TFieldDrawer snakeDrawer( snakeGame.gameField, renderer, mainFieldCellsGrid );
+    snakeDrawer.cellsMapper = snakeCellsMapper;
+
+    TFontTTF ttfTitleTextPrinter( renderer, fontFile, fontSize + 6 );
+    ttfTitleTextPrinter.setPoint( { background.width / 2, 10 }, TTextAlignment::centerAlignment );
+    TFontDrawer titleTextDrawer( ttfTitleTextPrinter );
+    titleTextDrawer.setText( "Snake" );
+
+    TFontTTF ttfScoreTextPrinter( renderer, fontFile, fontSize );
+    auto point = make_pair( activeGameField.xStart + ( cellsFieldParams.xCellsCount + 1 ) * cellsFieldParams.cellWidth + 20, gameFieldBound.yStart );
+    ttfScoreTextPrinter.setPoint( point, TTextAlignment::leftAlignment );
+    TFontDrawer scoreTextDrawer( ttfScoreTextPrinter );
+
+    TDrawer mainDrawer( renderer );
+    mainDrawer.addPrimitive( background );
+    mainDrawer.addPrimitive( gameFieldBound );
+    mainDrawer.addField( &snakeDrawer );
+    mainDrawer.addText( &scoreTextDrawer );
+    mainDrawer.addText( &titleTextDrawer );
+
+    snakeGame.mainDrawer = &mainDrawer;
+    snakeGame.scorePrinter = &scoreTextDrawer;
+
+    thread mainThr( &TSnakeGame::gameThread, &snakeGame );
+
+    snakeGame.ioThread();
+
+    mainThr.join();
+}
+
 void runTetris( TRenderer& renderer, TRectangleDescription& activeGameField, TRectangleDescription& background, const string& fontFile, size_t fontSize )
 {
     //! Cells params
@@ -160,56 +214,7 @@ int main( int argc, char **argv )
     TWindow wnd( "Main", background );
     TRenderer renderer( wnd );
 
-    //! Cells params
-    TCellsFieldParams cellsFieldParams;
-    cellsFieldParams.xCellsCount = 20;
-    cellsFieldParams.yCellsCount = 20;
-    cellsFieldParams.cellHeight = 25;
-    cellsFieldParams.cellWidth = 25;
-
-    TRectangleDescription gameFieldBound;
-    gameFieldBound.xStart = activeGameField.xStart - 1;
-    gameFieldBound.yStart = activeGameField.yStart - 1;
-    gameFieldBound.width = cellsFieldParams.xCellsCount * cellsFieldParams.cellWidth + 2;
-    gameFieldBound.height = cellsFieldParams.yCellsCount * cellsFieldParams.cellHeight + 2;
-    gameFieldBound.isFilled = false;
-    gameFieldBound.color = { 0xFF, 0xFF, 0xFF, 0xFF };
-
-    TCellRectangles mainFieldCellsGrid;
-    mainFieldCellsGrid.setCellsFieldParams( activeGameField, cellsFieldParams );
-    mainFieldCellsGrid.calcGrid();
-
-    //! Game backend
-    TSnakeGame snakeGame( { cellsFieldParams.yCellsCount, cellsFieldParams.xCellsCount }, 5 );
-
-    TFieldDrawer snakeDrawer( snakeGame.gameField, renderer, mainFieldCellsGrid );
-    snakeDrawer.cellsMapper = snakeCellsMapper;
-
-    TFontTTF ttfTitleTextPrinter( renderer, fontFile, fontSize + 6 );
-    ttfTitleTextPrinter.setPoint( { background.width / 2, 10 }, TTextAlignment::centerAlignment );
-    TFontDrawer titleTextDrawer( ttfTitleTextPrinter );
-    titleTextDrawer.setText( "Snake" );
-
-    TFontTTF ttfScoreTextPrinter( renderer, fontFile, fontSize );
-    auto point = make_pair( activeGameField.xStart + ( cellsFieldParams.xCellsCount + 1 ) * cellsFieldParams.cellWidth + 20, gameFieldBound.yStart );
-    ttfScoreTextPrinter.setPoint( point, TTextAlignment::leftAlignment );
-    TFontDrawer scoreTextDrawer( ttfScoreTextPrinter );
-
-    TDrawer mainDrawer( renderer );
-    mainDrawer.addPrimitive( background );
-    mainDrawer.addPrimitive( gameFieldBound );
-    mainDrawer.addField( &snakeDrawer );
-    mainDrawer.addText( &scoreTextDrawer );
-    mainDrawer.addText( &titleTextDrawer );
-
-    snakeGame.mainDrawer = &mainDrawer;
-    snakeGame.scorePrinter = &scoreTextDrawer;
-
-    thread mainThr( &TSnakeGame::gameThread, &snakeGame );
-
-    snakeGame.ioThread();
-
-    mainThr.join();
+    runSnake( renderer, activeGameField, background, fontFile, fontSize  );
 
     runTetris( renderer, activeGameField, background, fontFile, fontSize );
 
